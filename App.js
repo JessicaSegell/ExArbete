031ee82b641ed4774c94aa1b32b9c9ced378752b
JessicaSegell <44054186+JessicaSegell@users.noreply.github.com>
2020-05-01 23:37:16 +0200

    added new fonts. added icon. added styling to homescreen

--- App.js
+++ App.js
@@ -1,22 +1,41 @@
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, { useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { StyleSheet, Text, View } from 'react-native';
 import AppNavigator from './navigation/AppNavigator';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'kalam-bold': require('./assets/fonts/Kalam-Bold.ttf'),
+    'kalam-regular': require('./assets/fonts/Kalam-Regular.ttf'),
+  });
+};
 
 export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+      />
+    )
+  }
   return (
     <AppNavigator>
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
       </View>
-      </AppNavigator>
+    </AppNavigator>
   );
 }
 
 const styles = StyleSheet.create({
-        container: {
-        flex: 1,
+  container: {
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,15 +1,80 @@
 import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import { View, Text, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import styled from 'styled-components';
+
+
+const MenuItem = styled.TouchableOpacity`
+    width: 40%;
+    height: 25%;
+    margin: 10px;
+    background-color: white;
+    border-radius: 5px;
+    border: 2px;
+    border-color: black;
+`;
+
+const HeaderText = styled.Text`
+    color: white;
+    font-size: 28px;
+    font-family: kalam-bold;
+    margin: 5px;
+`;
+
+const SubHeaderText = styled.Text`
+    color: white;
+    font-size: 22px;
+    font-family: kalam-regular;
+    margin: 5px;
+    text-align: center;
+`;
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        backgroundColor: '#B4D0EA',
+    },
+    top: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    menuItem: {
+        flex: 2,
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        //alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
 
 const HomeScreen = ({ navigation }) => {
     return (
-        <View>
-            <Text>Home</Text>
+        <View style={styles.screen}>
+            {/* <Text>Home</Text>
             <TouchableOpacity onPress={() => navigation.navigate('Competition')}>
                 <Text>Play!</Text>
-            </TouchableOpacity>
+            </TouchableOpacity> */}
+            <View style={styles.top}>
+                <HeaderText>PLU Polarn</HeaderText>
+                <Image source={require('../assets/CashRegister.png')} />
+            </View>
+                <SubHeaderText>Välj en kategori</SubHeaderText>
+            <View style={styles.menuItem}>
+                <MenuItem>
+                    <Text>Styled</Text>
+                </MenuItem>
+                <MenuItem>
+                    <Text>Styled</Text>
+                </MenuItem>
+                <MenuItem>
+                    <Text>Styled</Text>
+                </MenuItem>
+                <MenuItem>
+                    <Text>Styled</Text>
+                </MenuItem>
+            </View>
         </View>
     )
-}; 
+};
 
 export default HomeScreen;
